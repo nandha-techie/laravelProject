9cb68709a8724bbbd378e75c3921c109c3928415
webapps\Perumal <nandhanandhu1991@gmail.com>
2017-04-02 20:39:12 -0700

    home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@extends('partials.header')
+
+@section('content')
+    <div class="container">
+      <h3>Collapsible Navbar</h3>
+        <p>In this example, the navigation bar is hidden on small screens and replaced by a button in the top right corner (try to re-size this window).
+        <p>Only when the button is clicked, the navigation bar will be displayed.</p>
+    </div>
+@endsection
