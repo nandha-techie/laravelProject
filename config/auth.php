f22c5f77231a5156d22a510ebb3d3b4a62ef3c7c
webapps\Perumal <nandhanandhu1991@gmail.com>
2017-04-04 21:00:52 -0700

    admin

--- config/auth.php
+++ config/auth.php
@@ -45,6 +45,10 @@ return [
             'driver' => 'token',
             'provider' => 'users',
         ],
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
     ],
 
     /*
